e5883d900b6e2f4643ca6d7f3e9826a008fe3865
tranthungan <you@example.com>
2018-08-18 10:14:00 +0700

    alo

---
+++ a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	Các bước đẩy câu lệnh file lên git
+	git+comit-m "Add navigation"
+	git push orgin master
+</body>
+</html>
